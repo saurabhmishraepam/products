6c65453c9fc217806cc52e09df42dece4608a0ee
saurabhmishraepam <mishra.saurabh19@gmail.com>
2019-03-05 13:19:22 +0530

    master branch

--- src/main/java/com/epam/productsreview/exceptionHadlers/BadProductRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.epam.productsreview.exceptionHadlers;
-
-public class BadProductRequestException extends RuntimeException{
-	private static final long serialVersionUID = 1778898889L;
-	public BadProductRequestException(String exceptionDetails) {
-		super(exceptionDetails);
-	}
-
-}
