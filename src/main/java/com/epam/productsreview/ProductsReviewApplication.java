74e60a4b98a524e9bc5e375dd65fa3edd48df4a0
saurabh <mishra.saurabh19@gmail.com>
2019-03-17 07:15:13 +0530

    week3task

--- src/main/java/com/epam/productsreview/ProductsReviewApplication.java
+++ src/main/java/com/epam/productsreview/ProductsReviewApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class ProductsReviewApplication {
 
 	public static void main(String[] args) {
